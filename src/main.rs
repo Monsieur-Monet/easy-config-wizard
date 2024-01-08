5f53865a4b1f6798526fc036c7e05c98bc0821ef
Monsieur-Monet <mail@tscheer.dev>
2024-01-08 15:58:33 +0100

    change name of function

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use webbrowser;
 
 slint::include_modules!();
 
-fn open_specific_url(url: &str) {
+fn open_url(url: &str) {
     if webbrowser::open(url).is_err() {
         println!("Failed to open url: {}", url);
     }
@@ -11,7 +11,7 @@ fn open_specific_url(url: &str) {
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    open_specific_url("https://www.rust-lang.org/learn/get-started");
+    open_url("https://www.rust-lang.org/learn/get-started");
 
     ui.run()
 }
