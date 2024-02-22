8afcc29d10349b9753708760ac07a139f1be369f
Monsieur-Monet <mail@tscheer.dev>
2024-02-22 17:17:05 +0100

    restructured app. Making this a single file app most likely because slint seems to have some limitations

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-use webbrowser;
+use webbrowser; // Assuming sidebar.slint is in the same directory
 
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
-    let welcome = AppWindow::new()?;
+    let ui = AppWindow::new()?;
 
-    welcome.run()
+    ui.on_button_pressed(move || {
+        webbrowser::open("https://www.rust-lang.org").unwrap();
+    });
+
+    ui.on_button2_pressed(move || {
+        webbrowser::open("https://www.google.com").unwrap();
+    });
+
+    ui.on_button3_pressed(move || {
+        webbrowser::open("https://www.github.com").unwrap();
+    });
+
+    ui.run()
 }
