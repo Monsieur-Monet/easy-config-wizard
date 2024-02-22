5aec49766da6fa629d10b47197d7ccc616cafd09
Monsieur-Monet <mail@tscheer.dev>
2024-02-22 18:22:32 +0100

    made navigation fully functional

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,47 @@
-use webbrowser; // Assuming sidebar.slint is in the same directory
+use webbrowser;
 
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    ui.on_button_pressed(move || {
-        webbrowser::open("https://www.rust-lang.org").unwrap();
+    // This needs to be refactored big time lol
+
+    let ui_weak1 = ui.as_weak();
+    let ui_weak2 = ui.as_weak();
+    let ui_weak3 = ui.as_weak();
+    let ui_weak4 = ui.as_weak();
+
+    ui.on_button1_pressed(move || {
+        let ui = ui_weak1.upgrade().unwrap();
+        let mut value = ui.get_currentView();
+        value = 1;
+        ui.set_currentView(value);
     });
 
     ui.on_button2_pressed(move || {
-        webbrowser::open("https://www.google.com").unwrap();
+        let ui = ui_weak2.upgrade().unwrap();
+        let mut value = ui.get_currentView();
+        value = 2;
+        ui.set_currentView(value);
     });
 
     ui.on_button3_pressed(move || {
-        webbrowser::open("https://www.github.com").unwrap();
+        let ui = ui_weak3.upgrade().unwrap();
+        let mut value = ui.get_currentView();
+        value = 3;
+        ui.set_currentView(value);
+    });
+
+    ui.on_button4_pressed(move || {
+        let ui = ui_weak4.upgrade().unwrap();
+        let mut value = ui.get_currentView();
+        value = 4;
+        ui.set_currentView(value);
+    });
+
+    ui.on_button5_pressed(move || {
+        webbrowser::open("https://swaywm.org/").unwrap();
     });
 
     ui.run()
