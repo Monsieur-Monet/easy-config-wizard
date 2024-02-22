5c13d8c59ce459e555b06496a99b6d6c5dca2e67
Monsieur-Monet <mail@tscheer.dev>
2024-02-22 16:34:48 +0100

    added sidebar navigation layout

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,8 @@ use webbrowser;
 
 slint::include_modules!();
 
-fn open_url(url: &str) {
-    if webbrowser::open(url).is_err() {
-        println!("Failed to open url: {}", url);
-    }
-}
-
 fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
+    let welcome = AppWindow::new()?;
 
-    open_url("https://www.rust-lang.org/learn/get-started");
-
-    ui.run()
+    welcome.run()
 }
