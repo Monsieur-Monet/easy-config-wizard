4be5b7f5577b75ef66aa740028c5e8f9b9571ec4
Monsieur-Monet <mail@tscheer.dev>
2024-03-13 16:40:27 +0100

    updating layout to use native tabs nad startet to push values into arrays to get them into the ui

--- src/main.rs
+++ src/main.rs
@@ -1,133 +1,71 @@
-use webbrowser;
+use std::array;
 
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
-    use std::fs;
     use std::fs::read_to_string;
     let ui = AppWindow::new()?;
 
-    // This needs to be refactored big time lol
+    let mut keybindings: Vec<String> = Vec::new();
+    let mut variables: Vec<String> = Vec::new();
+    let mut startup_programs: Vec<String> = Vec::new();
+    let mut window_borders: Vec<String> = Vec::new();
+    let mut window_gaps: Vec<String> = Vec::new();
+    let mut keyboard_layout: Vec<String> = Vec::new();
 
-    let ui_weak1 = ui.as_weak();
-    let ui_weak2 = ui.as_weak();
-    let ui_weak3 = ui.as_weak();
-    let ui_weak4 = ui.as_weak();
+    fn read_lines(filename: &str) -> Vec<String> {
+        read_to_string(filename)
+            .unwrap()
+            .lines()
+            .filter(|line| !line.trim().starts_with('#') && !line.trim().is_empty())
+            .map(|line| line.trim_start().to_string())
+            .collect()
+    }
+
+    for line in read_lines("/home/timo/.config/sway/config") {
+        if line.starts_with("bindsym") {
+            keybindings.push(line.to_string());
+        }
+    }
+
+    for line in read_lines("/home/timo/.config/sway/config") {
+        if line.starts_with("set") {
+            variables.push(line.to_string());
+        }
+    }
+
+    for line in read_lines("/home/timo/.config/sway/config") {
+        if line.starts_with("exec") || !line.starts_with("set") && line.ends_with("\\") {
+            // This is a janky way from preventing a line from the Variables section to be printed
+            startup_programs.push(line.to_string());
+        }
+    }
+
+    for line in read_lines("/home/timo/.config/sway/config") {
+        if line.contains("default") && line.contains("border") || line.contains("focused") {
+            window_borders.push(line.to_string());
+        }
+    }
+
+    for line in read_lines("/home/timo/.config/sway/config") {
+        if line.contains("gaps") {
+            window_gaps.push(line.to_string());
+        }
+    }
+
+    for line in read_lines("/home/timo/.config/sway/config") {
+        if line.contains("xkb_layout") {
+            keyboard_layout.push(line.to_string());
+        }
+    }
 
     ui.on_button1_pressed(move || {
-        let ui = ui_weak1.upgrade().unwrap();
-        let mut value = ui.get_currentView();
-        value = 1;
-        ui.set_currentView(value);
-    });
-
-    ui.on_button2_pressed(move || {
-        let ui = ui_weak2.upgrade().unwrap();
-        let mut value = ui.get_currentView();
-        value = 2;
-        ui.set_currentView(value);
-    });
-
-    ui.on_button3_pressed(move || {
-        let ui = ui_weak3.upgrade().unwrap();
-        let mut value = ui.get_currentView();
-        value = 3;
-        ui.set_currentView(value);
-    });
-
-    ui.on_button4_pressed(move || {
-        let ui = ui_weak4.upgrade().unwrap();
-        let mut value = ui.get_currentView();
-        value = 4;
-        ui.set_currentView(value);
-    });
-
-    ui.on_button5_pressed(move || {
-        webbrowser::open("https://swaywm.org/").unwrap();
-    });
-
-    ui.on_button6_pressed(move || {
-        webbrowser::open("https://github.com/Monsieur-Monet/easy-config-wizard").unwrap();
-    });
-
-    ui.on_button7_pressed(move || {
-        let data =
-            fs::read_to_string("/home/timo/.config/sway/config").expect("Unable to read file");
-        println!("{}", data);
-    });
-
-    ui.on_button8_pressed(move || {
-        fn read_lines(filename: &str) -> Vec<String> {
-            read_to_string(filename)
-                .unwrap()
-                .lines()
-                .filter(|line| !line.trim().starts_with('#') && !line.trim().is_empty())
-                .map(|line| line.trim_start().to_string())
-                .collect()
-        }
-
-        println!("Printing lines from sway config");
-        println!("");
-        println!("###############################################");
-        println!("Keybindings:");
-        println!("###############################################");
-        println!("");
-        for line in read_lines("/home/timo/.config/sway/config") {
-            if line.starts_with("bindsym") {
-                println!("{}", line);
-            }
-        }
-        println!("");
-        println!("###############################################");
-        println!("Variables:");
-        println!("###############################################");
-        println!("");
-        for line in read_lines("/home/timo/.config/sway/config") {
-            if line.starts_with("set") {
-                println!("{}", line);
-            }
-        }
-        println!("");
-        println!("###############################################");
-        println!("Startup Programs:");
-        println!("###############################################");
-        println!("");
-        for line in read_lines("/home/timo/.config/sway/config") {
-            if line.starts_with("exec") || !line.starts_with("set") && line.ends_with("\\") {
-                // This is a janky way from preventing a line from the Variables section to be printed
-                println!("{}", line);
-            }
-        }
-        println!("");
-        println!("###############################################");
-        println!("Window Borders:");
-        println!("###############################################");
-        println!("");
-        for line in read_lines("/home/timo/.config/sway/config") {
-            if line.contains("default") && line.contains("border") || line.contains("focused") {
-                println!("{}", line);
-            }
-        }
-        println!("");
-        println!("###############################################");
-        println!("Window Gaps:");
-        println!("###############################################");
-        println!("");
-        for line in read_lines("/home/timo/.config/sway/config") {
-            if line.contains("gaps") {
-                println!("{}", line);
-            }
-        }
-        println!("");
-        println!("###############################################");
-        println!("Keyboard Layout:");
-        println!("###############################################");
-        println!("");
-        for line in read_lines("/home/timo/.config/sway/config") {
-            if line.contains("xkb_layout") {
-                println!("{}", line);
-            }
-        }
+        println!("Keybindings: {:?}", keybindings);
+        println!("Variables: {:?}", variables);
+        println!("Startup Programs: {:?}", startup_programs);
+        println!("Window Borders: {:?}", window_borders);
+        println!("Window Gaps: {:?}", window_gaps);
+        println!("Keyboard Layout: {:?}", keyboard_layout);
     });
 
     ui.run()
